8227efcd3fe112f695db3cab0cb484b5f2bf796f
portkeyss <wudajing@gmail.com>
2024-04-27 22:56:22 -0500

    Time: 839 ms (65.16%), Space: 64.1 MB (16.29%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -3,19 +3,19 @@ class Solution:
         n = len(events)
         events.sort()
         hq = []
-        
-        minTime = min(events[i][0] for i in range(n))
-        maxTime = max(events[i][1] for i in range(n))
         ans = 0
         i = 0
-
-        for curTime in range(minTime,maxTime+1):
+        curTime = 0
+        while i<n or hq:
             while hq and hq[0]<curTime:
                 heapq.heappop(hq)
+            if not hq and i<n:
+                curTime = events[i][0]
             while i<n and events[i][0]==curTime:
                 heapq.heappush(hq, events[i][1])
                 i += 1
             if hq:
                 heapq.heappop(hq)
-                ans += 1  
-        return ans   
+                ans += 1
+                curTime += 1
+        return ans
