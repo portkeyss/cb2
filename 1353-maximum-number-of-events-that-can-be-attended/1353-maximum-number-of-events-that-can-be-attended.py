2a088c8865a2f88de5165ce2fc542274d6066944
portkeyss <wudajing@gmail.com>
2024-04-27 22:23:42 -0500

    Time: 856 ms (53.24%), Space: 64 MB (43.31%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -1,19 +1,21 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort(key=lambda x: (x[0], x[1]))
-        d = events[0][0]
-        last_day = max(events[idx][1] for idx in range(len(events)))
-        end_min_heap = []
+        n = len(events)
+        events.sort()
+        hq = []
+        
+        minTime = min(events[i][0] for i in range(n))
+        maxTime = max(events[i][1] for i in range(n))
+        ans = 0
         i = 0
-        res = 0
-        while d <= last_day:
-            while i < len(events) and events[i][0] == d:
-                heapq.heappush(end_min_heap, events[i][1])
+
+        for curTime in range(minTime,maxTime+1):
+            while hq and hq[0]<curTime:
+                heapq.heappop(hq)
+            while i<n and events[i][0]==curTime:
+                heapq.heappush(hq, events[i][1])
                 i += 1
-            if end_min_heap and end_min_heap[0] >= d:
-                res += 1
-                heapq.heappop(end_min_heap)
-            while end_min_heap and end_min_heap[0] == d:
-                heapq.heappop(end_min_heap)
-            d += 1    
-        return res   
+            if hq:
+                heapq.heappop(hq)
+                ans += 1  
+        return ans   
